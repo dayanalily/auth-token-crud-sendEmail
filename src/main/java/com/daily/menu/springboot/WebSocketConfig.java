d6b92604c1d4cbac054dadda513d6242a67b8c88
Dayanna Sanchez <dayanalily22@gmail.com>
2020-07-28 16:14:39 -0400

    mensja

--- src/main/java/com/daily/menu/springboot/WebSocketConfig.java
+++ src/main/java/com/daily/menu/springboot/WebSocketConfig.java
@@ -2,9 +2,11 @@ package com.daily.menu.springboot;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/chat-websocket")
-		.setAllowedOrigins("http://localhost:4200")
+		.setAllowedOrigins("https://edaily-spring.herokuapp.com")
 		.withSockJS();
 	}
 
